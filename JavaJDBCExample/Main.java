dc755e9d5ac353d0b08cc847ba8ce56998679ba4
Naina-coder28 <naina.24scse1410136@galgotiasuniversity.ac.in>
2025-11-21 15:26:30 +0530

    Add functionality to retrieve and display songs

--- JavaJDBCExample/Main.java
+++ JavaJDBCExample/Main.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SongDAO dao = new SongDAO();
+        ArrayList<Song> songs = dao.getAllSongs();
+
+        System.out.println("Songs in database:");
+        for (Song s : songs) {
+            System.out.println(s.getId() + " - " + s.getTitle() + " by " + s.getArtist());
+        }
+    }
+}
 

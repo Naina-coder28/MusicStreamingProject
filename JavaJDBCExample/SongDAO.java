d7ea3a306fcabc46a3e696cfb30010ece0049bcb
Naina-coder28 <naina.24scse1410136@galgotiasuniversity.ac.in>
2025-11-21 15:24:44 +0530

    Add getAllSongs method to SongDAO
    Implemented method to retrieve all songs from the database.

--- JavaJDBCExample/SongDAO.java
+++ JavaJDBCExample/SongDAO.java
@@ -1,1 +1,28 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class SongDAO {
+    
+    public ArrayList<Song> getAllSongs() {
+        ArrayList<Song> list = new ArrayList<>();
+        try {
+            Connection con = DatabaseConnection.getConnection();
+            String query = "SELECT * FROM songs";
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String artist = rs.getString("artist");
+
+                list.add(new Song(id, title, artist));
+            }
+        } catch (Exception e) {
+            System.out.println("Error fetching songs: " + e);
+        }
+        return list;
+    }
+}
 
